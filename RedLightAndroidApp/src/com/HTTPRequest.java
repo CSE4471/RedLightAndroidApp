e7e1c944fc7212aaa43a951507eaac4276fa0469
bbokerr6 <boker.6@osu.edu>
2013-03-24 19:02:46 -0300

    Added checking for values in range

--- RedLightAndroidApp/src/com/HTTPRequest.java
+++ RedLightAndroidApp/src/com/HTTPRequest.java
@@ -10,12 +10,13 @@ import com.github.kevinsawicki.http.HttpRequest;
 public class HTTPCaller {
  
   /**
-   * @param args
+	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String body = HttpRequest.get("http://redlights.herokuapp.com", true, "latitude", -40, "longitude", +83,"distance_in_miles",.0001).body();
-		body = body.substring(69); //may need to change this index if the header changes, and perhaps define it somewhere
+		//System.out.println(body);
+		//body = body.substring(69); //may need to change this index if the header changes, and perhaps define it somewhere
 		String longitude;
 		String latitude;//= t.substring(0, 10);
 		double lat = 0, lon=0;
@@ -57,18 +58,22 @@ public class HTTPCaller {
 					    catch (NumberFormatException e){
 					    System.out.println("NumberFormatException: " + e.getMessage());
 				  }
+				  if (Math.abs(lat)<=90 && Math.abs(lon)<=180)
+				  {/*create latLng and add to array*/}
+				  
+				  
 					  
 				  //NEED TO CONSTRUCT latlng objects, place into an array, and make this array available to other classes
 				  //this is inside of the loop where the values are found and made into doubles.  each iteration, at this point
 				  //the doubles need to be placed into the latlng object and this needs to be added to the array (or other similar structure)
 				  
 				  
-				/*try{	
+				try{	
 				System.out.println("latitude: " + lat + "longitude: " + lon);
 				}
 					finally{
 						lat=0;
 						lon=0;
-					}*/
+					}
 	}
 }}
